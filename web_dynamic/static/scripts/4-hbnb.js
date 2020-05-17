2e6b9f145343f963bf22994ec35f9da3e84a11b5
Jose David Caro <j.caro0207@gmail.com>
2020-05-17 14:56:54 -0500

    [Update 4-hbnb.js]

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -1,5 +1,6 @@
-$(function () {
+$(document).ready(function () {
   const amenDict = {};
+  const amenFilter = {};
   $('input.check_amenity').click(function () {
     if ($(this).prop('checked') === true) {
       const amenName = $(this).attr('data-name');
@@ -15,6 +16,7 @@ $(function () {
     }
   });
 
+
   const urlStatus = 'http://0.0.0.0:5001/api/v1/status/';
   $.get(urlStatus, function (data) {
     if (data.status === 'OK') {
@@ -25,61 +27,68 @@ $(function () {
   });
 
   const urlPlaces = 'http://0.0.0.0:5001/api/v1/places_search/';
-  $.ajax({
-    type: 'POST',
-    url: urlPlaces,
-    data: JSON.stringify({'amenities': Object.keys(amenDict).join(', ')}),
-    contentType: 'application/json'
-  })
-    .done(function (places) {
-      for (let i = 0; i < places.length; i++) {
-        const article = document.createElement('article');
+  function get_places() {
+    console.log(JSON.stringify(amenFilter));
+    $.ajax({
+      type: 'POST',
+      url: urlPlaces,
+      data: JSON.stringify(amenFilter),
+      contentType: 'application/json'
+    })
+      .done(function (places) {
+	$('section.places').empty();
+	for (let i = 0; i < places.length; i++) {
+          const article = document.createElement('article');
 
-        const divTitle = document.createElement('div');
+          const divTitle = document.createElement('div');
 
-        const h2PlaceName = document.createElement('h2');
-        divTitle.classList.add('title_box');
-        h2PlaceName.innerHTML = places[i].name;
+          const h2PlaceName = document.createElement('h2');
+          divTitle.classList.add('title_box');
+          h2PlaceName.innerHTML = places[i].name;
 
-        const divPrice = document.createElement('div');
-        divPrice.classList.add('price_by_night');
-        divPrice.innerHTML = '$' + places[i].price_by_night;
+          const divPrice = document.createElement('div');
+          divPrice.classList.add('price_by_night');
+          divPrice.innerHTML = '$' + places[i].price_by_night;
 
-        divTitle.append(h2PlaceName, divPrice);
+          divTitle.append(h2PlaceName, divPrice);
 
-        const divInformation = document.createElement('div');
-        divInformation.classList.add('information');
+          const divInformation = document.createElement('div');
+          divInformation.classList.add('information');
 
-        const divMaxGuest = document.createElement('div');
-        divMaxGuest.classList.add('max_guest');
-        const maxGuest = places[i].max_guest;
-        const htmlGuest = maxGuest > 1 ? ' Guests' : ' Guest';
-        divMaxGuest.innerHTML = maxGuest + htmlGuest;
+          const divMaxGuest = document.createElement('div');
+          divMaxGuest.classList.add('max_guest');
+          const maxGuest = places[i].max_guest;
+          const htmlGuest = maxGuest > 1 ? ' Guests' : ' Guest';
+          divMaxGuest.innerHTML = maxGuest + htmlGuest;
 
-        const divNumRooms = document.createElement('div');
-        divNumRooms.classList.add('number_rooms');
-        const numRooms = places[i].number_rooms;
-        const htmlRooms = numRooms > 1 ? ' Rooms' : ' Room';
-        divNumRooms.innerHTML = numRooms + htmlRooms;
+          const divNumRooms = document.createElement('div');
+          divNumRooms.classList.add('number_rooms');
+          const numRooms = places[i].number_rooms;
+          const htmlRooms = numRooms > 1 ? ' Rooms' : ' Room';
+          divNumRooms.innerHTML = numRooms + htmlRooms;
 
-        const divNumBaths = document.createElement('div');
-        divNumBaths.classList.add('number_bathrooms');
-        const numBaths = places[i].number_bathrooms;
-        const htmlBaths = numBaths > 1 ? ' Bathrooms' : ' Bathrooms';
-        divNumBaths.innerHTML = numBaths + htmlBaths;
+          const divNumBaths = document.createElement('div');
+          divNumBaths.classList.add('number_bathrooms');
+          const numBaths = places[i].number_bathrooms;
+          const htmlBaths = numBaths > 1 ? ' Bathrooms' : ' Bathrooms';
+          divNumBaths.innerHTML = numBaths + htmlBaths;
 
-        divInformation.append(divMaxGuest, divNumRooms, divNumBaths);
+          divInformation.append(divMaxGuest, divNumRooms, divNumBaths);
 
-        const divDescription = document.createElement('div');
-        divDescription.classList.add('description');
-        divDescription.innerHTML = places[i].description;
+          const divDescription = document.createElement('div');
+          divDescription.classList.add('description');
+          divDescription.innerHTML = places[i].description;
 
-        article.append(divTitle, divInformation, divDescription);
-        $('section.places').append(article);
-      }
+          article.append(divTitle, divInformation, divDescription);
+          $('section.places').append(article);
+	}
+      });
+    	$('section.places').load(' section.places');
+  }
+    get_places();
+  $('section.filters button').click(function () {
+    amenFilter['amenities'] = Object.keys(amenDict);
+    get_places();
     });
 
-    $('section.filters button').click(function () {
-
-    });
 });
